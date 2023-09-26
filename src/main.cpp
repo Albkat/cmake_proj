aa45dc10184fc34fd16ca8fa4a7230ffecbf880c
albert <92109627+Albkat@users.noreply.github.com>
2023-09-26 17:39:25 +0200

    add additional text
    Signed-off-by: albert <92109627+Albkat@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,30 @@
 
 int main() {
 
-   int low,up; // specify the range
+   int low, up; // specify the range
+   int buff;
 
    // user interaction
    std :: cout << "Welcome to the game!" << std::endl;
    std :: cout << "Please enter limits (e.g. from 1 to 49)" << std::endl;
    std :: cin >> low >> up;
 
+   // small check if numbers are in the right order
+   if (low > up) {
+      buff = low;
+      low = up;
+      up = buff;
+   } else if (low == up) {
+      std::cout << "The same number entered twice! Abort ..." <<std::endl;
+      return 1;
+   }
+
+   std::cout << "Let's start!" << std::endl;
+   std::cout << "I am thinking of a number b/n " << low << " and " << up << std::endl;
+
    // create a game instance
    GuessingGame game(low,up);
+
 
    return 0;
 }
