28720e096ef800fb395075bb7d7192f898e326cb
albert <92109627+Albkat@users.noreply.github.com>
2023-10-13 18:01:40 +0200

    refactor CMake scripts
    Signed-off-by: albert <92109627+Albkat@users.noreply.github.com>

---
+++ cmake_config/modules/Findgit.cmake
@@ -0,0 +1,11 @@
+# define custom module to find the lol package
+
+# set package-specific variables
+set(_lib "git")
+set(_pkg "git_learn")
+set(_url "https://github.com/Albkat/git_learn")
+
+# check if the package is found
+if(NOT DEFINED "${_pkg}_FIND_METHOD")
+
+endif()
